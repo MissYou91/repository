587a5fe99ec097327ce3db453f96d94df28fe4f3
rains2711 <271102154@qq.com>
2019-12-07 16:18:06 +0800

    粟鹏：上传开发需求文件

--- mall-web/src/main/java/com/wx/disparent/mallparent/controller/IndexControl.java
+++ mall-web/src/main/java/com/wx/disparent/mallparent/controller/IndexControl.java
@@ -1,8 +1,14 @@
 package com.wx.disparent.mallparent.controller;
 
+import com.wx.disparent.mallparent.entity.UserEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/f")
@@ -10,5 +16,10 @@ public class IndexControl {
     @RequestMapping("/ff")
     public String geta(){
         return "index";
+    }
+    @RequestMapping("/u")
+    public String getu(){
+        System.out.println(11);
+        return "user/user";
     }
 }
